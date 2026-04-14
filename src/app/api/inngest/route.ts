21ce98aa8c859074d40c580a5b1b0b52b51f45c7
pieroangulocubas <pieroangulocubas.17@gmail.com>
2026-04-14 15:15:20 +0200

    chore(inngest): set maxDuration to 300s

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { serve } from "inngest/next"
 import { inngest } from "@/inngest/client"
 import { analizarClasificador } from "@/inngest/functions/clasificador"
 
+export const maxDuration = 300
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [analizarClasificador],
